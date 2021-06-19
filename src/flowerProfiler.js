b266e222439ef72630a3bebde2cd11082dd1139a
B. Ruschill <brandon.ruschill@gmail.com>
2021-06-19 16:36:35 -0500

    add initial state for profiler

--- src/flowerProfiler.js
+++ src/flowerProfiler.js
@@ -1,9 +1,29 @@
+import React, { setState } from 'react';
 import { list as questionList } from "./question/lists";
 import { optionalList as optionalQuestionList } from "./question/lists";
 import { Fragment } from "react";
 
+const INITIAL_STATE = {
+  flowerType: '',
+  flowerSex: '',
+  sepalType: '',
+  sepalCount: '',
+  sepalColor: '',
+  petalType: '',
+  petalCount: '',
+  petalColor: '',
+  stamenType: '',
+  stamenCount: '',
+  stamenCountIsNumerous: false,
+  stamenArrangement: '',
+  chamberCount: '',
+  carpelCount: '',
+  styleCount: '',
+  stigmaLobeCount: ''
+}
+
 export const FlowerProfiler = (props) => {
-  let [profile, setProfile] = setState({})
+  let [profile, setProfile] = setState(INITIAL_STATE);
 
   return (
     <Fragment>
